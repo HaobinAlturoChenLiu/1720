d45a93f7ecb3556a42fe952b2af8284bf51763fa
HaobinAlturoChenLiu <77351345+HaobinAlturoChenLiu@users.noreply.github.com>
2022-03-28 19:10:31 -0400

    Update app.py

--- sample_site/app.py
+++ sample_site/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import test
 
 app = Flask(__name__)
 
@@ -14,6 +15,19 @@ def index():
     f.close()
 
     return render_template("index.html", count=count)
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == "POST":
+        f = open("count.txt", "r")
+        count = str(f.read())
+        f.close()
+
+        f = open("count.txt", "w")
+        f.write(str(count+ "hello"))
+        f.close()
+
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
