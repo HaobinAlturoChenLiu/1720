c7524aa892c505a8d9ff49f19482fb5d5b25ce6a
HaobinAlturoChenLiu <77351345+HaobinAlturoChenLiu@users.noreply.github.com>
2022-03-28 18:12:28 -0400

    Update app.py

--- sample_site/app.py
+++ sample_site/app.py
@@ -7,15 +7,14 @@ def index():
 
     # Load current count
     f = open("count.txt", "r")
-    count = int(f.read())
+    count = str(f.read())
     f.close()
 
     # Increment the count
-    count += 1
 
     # Overwrite the count
     f = open("count.txt", "w")
-    f.write(str('hello'))
+    f.write(str(count+'hello'))
     f.close()
 
     # Render HTML with count variable
