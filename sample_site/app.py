873f527379a179702d04a172f5eff6bbc0c9df36
HaobinAlturoChenLiu <77351345+HaobinAlturoChenLiu@users.noreply.github.com>
2022-03-28 19:09:29 -0400

    Update app.py

--- sample_site/app.py
+++ sample_site/app.py
@@ -15,19 +15,6 @@ def index():
 
     return render_template("index.html", count=count)
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == "POST":
-        f = open("count.txt", "r")
-        count = str(f.read())
-        f.close()
-
-        f = open("count.txt", "w")
-        f.write(str(count+ "hello"))
-        f.close()
-
-    return render_template('index.html')
-
 if __name__ == "__main__":
     app.run()
     
