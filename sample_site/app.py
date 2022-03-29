642007e90a879d4bf09d09bf59ea4ccd589781cb
HaobinAlturoChenLiu <77351345+HaobinAlturoChenLiu@users.noreply.github.com>
2022-03-28 20:15:28 -0400

    Update app.py

--- sample_site/app.py
+++ sample_site/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/")
 def index():
     
     f = open("count.txt", "r")
@@ -10,18 +10,6 @@ def index():
     f.close()
 
     f = open("count.txt", "w")
-    f.close()
-    
-    output = request.form.to_dict()
-    print(output)
-    name = output["name"]
-    
-    f = open("count.txt", "r")
-    count = str(f.read())
-    f.close()
-
-    f = open("count.txt", "w")
-    f.write(str(name + " " + count+ "\n"))
     f.close()
 
     return render_template("index.html", count=count)
