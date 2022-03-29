5abcd1d241eacbe7f0fd7c03ffad1fb2ef3614c8
HaobinAlturoChenLiu <77351345+HaobinAlturoChenLiu@users.noreply.github.com>
2022-03-28 20:17:33 -0400

    Update app.py

--- sample_site/app.py
+++ sample_site/app.py
@@ -29,7 +29,7 @@ def result():
     f.close()
 
     f = open("count.txt", "w")
-    f.write(str(name + " " + count+ "\n"))
+    f.write(str(name + " " + count))
     f.close()
     
     return render_template('index.html', name = name)
